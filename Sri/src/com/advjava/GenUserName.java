ba1c93404f1e756e4ec78a72593b3fbeaaf67507
Freeware Sys <Freeware Sys@Srilekha>
2017-09-20 08:19:53 -0700

    First commit

---
+++ Sri/src/com/advjava/GenUserName.java
@@ -0,0 +1,29 @@
+package com.advjava;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/*
+ * Servlet implementation class GenUserName
+ */
+@WebServlet("/loginpage")
+public class GenUserName extends HttpServlet {
+	public void service(HttpServletRequest req,HttpServletResponse res) throws IOException{
+		PrintWriter pw=res.getWriter();
+		String FirstName=req.getParameter("First");
+		String LastName=req.getParameter("Last");
+		int age=Integer.parseInt(req.getParameter("Age"));
+		pw.println("<html>");
+		pw.println("<body style=\"background-color:green;color:blue;\">");
+		pw.println("UserId:"+FirstName.substring(0,3)+age+LastName);
+		pw.println("</body>");
+		pw.println("</html>");
+		
+	}
+
+}
